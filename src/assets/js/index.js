7b05b6a34a593a143ff7358f999544bd6a850b26
regniek <juanespitia@outlook.com>
2021-01-24 20:38:06 -0500

    inital responsive

---
+++ src/assets/js/index.js
@@ -0,0 +1,17 @@
+
+function hideShow(menuId,guitarId,closeId){
+
+    const menu=document.querySelector(menuId);
+    const guitar=document.querySelector(guitarId);
+    const close=document.querySelector(closeId);
+    if(menu.classList.contains('aparece')){
+        menu.classList.remove('aparece');
+        close.classList.remove('aparece')
+        guitar.classList.add('esconde')
+    }else{
+        menu.classList.add('aparece');
+        close.classList.add('aparece')
+        guitar.classList.remove('esconde')
+    }
+
+}
